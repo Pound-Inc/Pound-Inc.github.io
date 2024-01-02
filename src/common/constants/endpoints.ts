f8aad084a55e8838fe7558b132b63b18d8b8a4f6
Martin Abdulghni <martin.abdulghni@navcite.se>
2024-01-02 02:06:01 +0100

    a

--- src/common/constants/endpoints.ts
+++ src/common/constants/endpoints.ts
@@ -3,3 +3,4 @@ export const PROGRAMS_API = 'http://k8s-default-pounding-eeba42606a-2115020394.e
 export const ORDERS_API = 'http://k8s-default-pounding-eeba42606a-2115020394.eu-north-1.elb.amazonaws.com/orders';
 export const PLANS_API = 'http://k8s-default-pounding-eeba42606a-2115020394.eu-north-1.elb.amazonaws.com/program';
 export const IMGBB_API = 'https://api.imgbb.com/1/upload';
+ 
