6d3700f62e7fc96ba3faca02b40fbb293cea41fe
Martin Abdulghni <martin.abdulghni@navcite.se>
2024-01-02 02:05:22 +0100

    a

--- src/common/constants/endpoints.ts
+++ src/common/constants/endpoints.ts
@@ -1,5 +1,5 @@
-export const AUTH_API = 'http://localhost:3001/auth';
-export const PROGRAMS_API = 'http://localhost:3010/programs';
-export const ORDERS_API = 'http://localhost:3000/orders';
-export const PLANS_API = 'http://localhost:3010/program';
+export const AUTH_API = 'http://k8s-default-pounding-eeba42606a-2115020394.eu-north-1.elb.amazonaws.com/auth';
+export const PROGRAMS_API = 'http://k8s-default-pounding-eeba42606a-2115020394.eu-north-1.elb.amazonaws.com/programs';
+export const ORDERS_API = 'http://k8s-default-pounding-eeba42606a-2115020394.eu-north-1.elb.amazonaws.com/orders';
+export const PLANS_API = 'http://k8s-default-pounding-eeba42606a-2115020394.eu-north-1.elb.amazonaws.com/program';
 export const IMGBB_API = 'https://api.imgbb.com/1/upload';
